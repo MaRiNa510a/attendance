e8415001cb7c2784e66449a2d03eca6d4d7e9a95
Marina Goto <5m.clover3@gmail.com>
2018-04-29 08:17:53 +0900

    Initial Commit

---
+++ attendance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  attendance
+//
+//  Created by Marina Goto on 2018/04/29.
+//  Copyright © 2018年 lifeistech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
